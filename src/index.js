d9d0456702d9d229e4a96b1c5e2c192b774bbe45
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2022-10-09 19:32:21 +0300

    change pagination

--- src/index.js
+++ src/index.js
@@ -6,16 +6,8 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // autoHeight: true,
 });
