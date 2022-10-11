0fc9dc8d608ace0fdc1952afdc110edf80a33abb
KuznietsovaOksana <109915545+KuznietsovaOksana@users.noreply.github.com>
2022-10-11 12:51:45 +0000

    Deploying to gh-pages from @ KuznietsovaOksana/team-project@2073c8bc4a7853c582ceb230640d25d6cd4fb409 🚀

---
+++ index.b225583a.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,spaceBetween:24,pagination:{el:".swiper-pagination",clickable:!0},autoHeight:!0,grabCursor:!0}),new Swiper(".swiper_02",{direction:"horizontal",loop:!0,spaceBetween:24,autoplay:{delay:3e3,disableOnInteraction:!1}});
+//# sourceMappingURL=index.b225583a.js.map
